0bd32e80c191ff75f21b00eda9508e5a4415767b
akshay nagargoje <akshay.nagargoje@myrl.tech>
2024-07-23 11:46:02 +0530

    Update demo.sol

--- demo.sol
+++ demo.sol
@@ -1,1 +1,55 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string public greeting;
+
+    constructor() {
+        greeting = "Hello, World!";
+    }
+
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+
+    function getGreeting() public view returns (string memory) {
+        return greeting;
+    }
+}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string public greeting;
+
+    constructor() {
+        greeting = "Hello, World!";
+    }
+
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+
+    function getGreeting() public view returns (string memory) {
+        return greeting;
+    }
+}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string public greeting;
+
+    constructor() {
+        greeting = "Hello, World!";
+    }
+
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+
+    function getGreeting() public view returns (string memory) {
+        return greeting;
+    }
+}
